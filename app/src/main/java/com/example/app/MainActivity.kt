78cc87ec4315df40d9e8be0651e97ffb5736b13e
Diego Sisternes <daigos.sd@gmail.com>
2022-02-25 18:09:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //Iniciar activity
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
