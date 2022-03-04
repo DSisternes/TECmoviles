e5b759c5338deca187133038121304578a9d49f5
Diego Sisternes <daigos.sd@gmail.com>
2022-03-04 00:26:37 -0600

    Nueva branch. Práctica de ciclos básicos para Kotlin.

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,31 +2,95 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    val constante: String = "una constante"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a: Int = 1
+        //a = 5
+
+        /*Comentario
+        varias
+        lineas
+         */
+
+        var b: Int = 2
+        b = 3
+
+        Log.i("Basics", "Valor de b $b")
+        Toast.makeText(this, "Valor de b = $b", Toast.LENGTH_LONG).show()
+        Log.i("Basics", constante)
+        Log.d("Basics", "d")
+        Log.w("Basics", "w")
+        Log.e("Basics", "e")
+
+        unSwitch()
+        unIf(1)
+        unFor()
+        unosWhile()
+    }
+    fun unSwitch(){
+        val x = 1
+
+        when (x) {
+            1 -> Log.i("Basics", "Es un uno")
+            2 -> {
+                var algo = "lorem"
+                var otro = "ipsum"
+                Log.e("Basics", algo + otro)
+            }
+            else -> Log.e("Basics", "No coincidio")
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        //Iniciar activity
+    fun unIf(a: Int): Boolean{
+        var array = arrayOf(1,2,3,5,8,10)
+        if(a in array) //&& || != < > ==
+            return true
+        else
+            return false
+
     }
 
-    override fun onPause() {
-        super.onPause()
+    fun unFor() {
+        var array = arrayOf(1, 2, 3, 4, 5,8,10)
+        //for (numero in array.indices){
+        Log.i("Basics", "numero in array")
+        for (numero in array) {
+            Log.i("Basics", numero.toString())
+        }
+
+        Log.i("Basics", "1..3")
+        for (i in 1..3) {
+            Log.i("Basics", i.toString())
+        }
+
+        Log.i("Basics", "6 downTo 0 step 3")
+        for (i in 6 downTo 0 step 3) {
+            Log.i("Basics", i.toString())
+        }
     }
 
-    override fun onStop() {
-        super.onStop()
-    }
+    fun unosWhile(){
+        var x = 0
+        while(x < 5){
+            Log.i("Basics", x.toString())
+            x++
+        }
 
-    override fun onResume() {
-        super.onResume()
-    }
+        x = 0
+        do{
+            Log.i("Basics", x.toString())
+            x++
+        }while(x < 5)
 
-    override fun onDestroy() {
-        super.onDestroy()
     }
 }
